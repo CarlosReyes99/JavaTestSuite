3e15672a983ad5fe8e598a4b21236108241724e6
Carlos Reyes <carlosmxm055@gmail.com>
2023-10-31 19:48:10 -0600

    test to simulate payment gateways working

---
+++ src/test/java/payments/PaymentProcessorTest.java
@@ -0,0 +1,39 @@
+package payments;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+
+    @Before
+    public void setUp() throws Exception {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
+
+
+    @Test
+    public void payment_is_correct() {
+
+        Mockito.when(paymentGateway.requestPayment(Mockito.<PaymentRequest>any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
+
+        assertTrue(paymentProcessor.makePayment(1000));
+    }
+    @Test
+    public void payment_is_wrong() {
+
+        Mockito.when(paymentGateway.requestPayment(Mockito.<PaymentRequest>any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
+
+        assertFalse(paymentProcessor.makePayment(1000));
+    }
+}
